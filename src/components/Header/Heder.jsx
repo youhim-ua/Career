df19cadd3dd5f993df371dcd53b8cc0dce52a60b
Andrew Chepur-Krytskyi <66245509+youhim-ua@users.noreply.github.com>
2021-10-24 12:56:59 +0300

    functionality to hide header + throttle it

--- src/components/Header/Heder.jsx
+++ src/components/Header/Heder.jsx
@@ -1,6 +1,32 @@
+import { useState, useEffect } from "react";
+import _ from "lodash";
+
 const Header = ({ children }) => {
-  return (
-    <header className="header">
+  const [showHeader, setShowHeader] = useState(true);
+
+  useEffect(() => {
+    let startScrollHeight = window.pageYOffset;
+
+    const scrollHandler = () => {
+      const lastScrollHeight = window.pageYOffset;
+      if (startScrollHeight < lastScrollHeight) {
+        setShowHeader(false);
+      } else {
+        setShowHeader(true);
+      }
+      startScrollHeight = lastScrollHeight;
+    }
+    
+    document.addEventListener("scroll", _.throttle(scrollHandler, 400));
+
+    return () => {
+      document.removeEventListener("scroll", _.throttle(scrollHandler, 400));
+    }
+  }, [])
+
+
+  return ( 
+    <header className={ showHeader ? "header" : "header header--hide" }>
       <div>
         { children }
       </div>
